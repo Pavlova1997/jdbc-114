e7bfc978b6958d18cb801ee4aaa2becc86fd3438
Pavlova1997 <142187250+Pavlova1997@users.noreply.github.com>
2024-05-31 22:10:30 +0300

    Add files via upload

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,20 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
